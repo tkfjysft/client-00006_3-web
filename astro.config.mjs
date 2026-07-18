712cec06ce2a008fcf567adf23d563a3afed925e
tkfjysft <tkfjysft.2013.frgn@gmail.com>
2026-07-18 21:05:19 +0900

    feat:cloudflareにデプロイ3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-import cloudflare from '@astrojs/cloudflare';
-
-// https://astro.build/config
 export default defineConfig({
-  //   base: '/single/',
   outDir: './dist',
-
-  site: 'https://sws.studio-condor.com/',
-
   vite: {
     plugins: [tailwindcss()]
   },
-
-  adapter: cloudflare()
+  output: 'static', // これだけでOK
 });
