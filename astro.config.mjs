638b4c06ee45729e38439c09c910d692f937ead7
tkfjysft <tkfjysft.2013.frgn@gmail.com>
2026-08-01 05:52:55 +0900

    fix:サイト紐づけ修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   outDir: './dist',
+  // すべてのアセット参照を相対パス（./）にする
+  base: './',
   vite: {
     plugins: [tailwindcss()]
   },
-  output: 'static', // これだけでOK
+  output: 'static',
 });
